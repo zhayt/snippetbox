7ceafa3564130aa0b1e2fdf8fe23caff3cc9e92b
Aybek <turk.883ar@gmail.com>
2023-01-18 20:17:42 -0800

    Logging. Create two new custom loggers for info and error information

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":9000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Starting server on :9000")
-	err := http.ListenAndServe(":9000", mux)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
-		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 }
