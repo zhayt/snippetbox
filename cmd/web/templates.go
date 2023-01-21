51a0c3939e19dda862474a3cf5ff888e22241b7b
Aybek <turk.883ar@gmail.com>
2023-01-21 03:43:11 -0800

    Format date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"github.com/zhayt/snippetbox-full-version/pkg/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -22,7 +31,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
