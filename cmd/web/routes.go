808f5cfddcbeb528cdebd35939c711cc6097c915
Aybek <turk.883ar@gmail.com>
2023-01-21 22:30:19 -0800

    Made middelware for secureHeaders, log request and recover panic

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,12 +2,12 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
-	return mux
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
