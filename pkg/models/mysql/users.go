83b80c4f1d8a9ed9865ac5d690394c582a59a052
Aybek <turk.883ar@gmail.com>
2023-01-24 23:37:00 -0800

    Update user Insert method

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -3,6 +3,10 @@ package mysql
 import (
 	"database/sql"
 	"github.com/zhayt/snippetbox-full-version/pkg/models"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -11,7 +15,24 @@ type UserModel struct {
 
 // Insert use to add a new record to the users table.
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+	// Create a bcrypt hash of the plain-text password.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+	return err
 }
 
 // Authenticate use to verify whether a user exists with the provided email address and password.
