c13ac4961a91b1aad9cd3df801aa290d4438fe64
Aybek <turk.883ar@gmail.com>
2023-01-25 01:25:39 -0800

    Update user Authenticate method

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -38,7 +38,29 @@ func (m *UserModel) Insert(name, email, password string) error {
 // Authenticate use to verify whether a user exists with the provided email address and password.
 // This will return the relevant user iD if they do.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	row := m.DB.QueryRow("SELECT id, hashed_password FROM users WHERE email = ?")
+
+	err := row.Scan(&id, &hashedPassword)
+	if err == sql.ErrNoRows {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	// Check whether the hashed password and plain-text password provided match
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	// Return user id if we don't have eny error
+	return id, nil
 }
 
 // Get use to fetch details for a specific user based on their user ID.
