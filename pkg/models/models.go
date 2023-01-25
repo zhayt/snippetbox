0afa70de0e267fbc69584fc3a79e1e8654974be7
Aybek <turk.883ar@gmail.com>
2023-01-24 21:47:14 -0800

    Add new model for users

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,13 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// ErrInvalidCredentials use when user enter an incorrect email addr
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// ErrDuplicateEmail use when user enter email addr that has already exist
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 type Snippet struct {
 	ID      int
@@ -13,4 +19,12 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
 }
