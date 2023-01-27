9868afcf31d1768274c7ee24aa6b84c1a10ee1d1
Aybek <turk.883ar@gmail.com>
2023-01-27 05:24:04 -0800

    Mocking Dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golangcollege/sessions"
+	"github.com/zhayt/snippetbox-full-version/pkg/models"
 	"github.com/zhayt/snippetbox-full-version/pkg/models/mysql"
 	"html/template"
 	"log"
@@ -19,12 +20,20 @@ type contextKey string
 var contextKeyUser = contextKey("user")
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
---
+++ cmd/web/testtutils_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"github.com/golangcollege/sessions"
+	"github.com/zhayt/snippetbox-full-version/pkg/models/mock"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+// newTestApplication returns an instance of application struct containing mocked dependencies
+func newTestApplication(t *testing.T) *application {
+	// Create an instance of the template cache.
+	templateCache, err := newTemplateCache("./../../ui/html/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a session manager instance, with the same settings as production.
+	session := sessions.New([]byte("urveiuo4fo446sdvsdcADNJKdc"))
+	session.Lifetime = 12 * time.Hour
+	session.Secure = true
+
+	// Initialize the dependencies, using the mocks the loggers and
+	// database models.
+	return &application{
+		errorLog:      log.New(ioutil.Discard, "", 0),
+		infoLog:       log.New(ioutil.Discard, "", 0),
+		session:       session,
+		snippets:      &mock.SnippetModel{},
+		templateCache: templateCache,
+		users:         &mock.UserModel{},
+	}
+}
+
+// testServer anonymously embeds a httptest.Server instance.
+type testServer struct {
+	*httptest.Server
+}
+
+// newTestServer helper which initializes and returns a new instance of our custom testServer type
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+
+	// Initialize a new cookie jar
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the cookie jar to the client, so that response cookies are stored
+	// and then sent with subsequent requests
+	ts.Client().Jar = jar
+
+	// Disable redirect-following for the client. Essentially this function
+	// is called after a 3xx response is received by the client, and returning
+	// the http.ErrUseLastResponse error forces it to immediately return the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &testServer{ts}
+}
+
+// get method on our custom testServer type.
+// This makes a GET request to a given url path on the test server, and returns the response
+// status code, headers and body
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
+}
